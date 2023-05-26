0d3b469cef0c3d2aa48205d9604079aae6142db8
mahedi6666655555 <mahedinir34678@gmail.com>
2023-05-26 16:56:59 +0600

    place holdre make white done

--- src/Components/From.jsx
+++ src/Components/From.jsx
@@ -1,13 +1,15 @@
-import {FaPhone, FaSearchLocation } from "react-icons/fa";
+import { FaPhone, FaSearchLocation } from "react-icons/fa";
+
+import '../css/banner.css'
 
 const From = () => {
     return (
         <div>
             <div className=" bg-[#07332F] flex justify-center items-center w-full h-[528px]">
 
-                <div className="flex w-full ">
+                <div className="flex justify-center gap-20  w-full ">
 
-                    <div className=" ml-4 text-white w-1/2 ">
+                    <div className=" ml-4 text-white  ">
                         <h1 className="text-4xl font-bold ">Contact With Us</h1>
 
                         <p className="py-4">Sed ut perspiciatis unde omnis iste natus error sit <br /> voluptatem accusantium doloremque laudantium, <br /> totam rem aperiam, eaque ipsa quae ab illo inve ntore <br /> veritatis et quasi.</p>
@@ -19,13 +21,13 @@ const From = () => {
                         </div>
 
                     </div>
-                    <div className="grid mr-4 grid-cols-2 gap-4  w-1/2">
-                        <input className=" pl-2 rounded-md w-[340px] py-5 " type="text" placeholder="name" />
-                        <input className=" pl-2 rounded-md w-[340px] py-5 " type="text" placeholder="email" />
-                        <input className=" pl-2 rounded-md w-[340px] py-5 " type="text" placeholder="Mobile number" />
-                        <input className=" pl-2 rounded-md w-[340px] py-5 " type="text" placeholder="Doctor Name" />
-                        <input className=" px-2 rounded-md w-[340px] py-5 " type="date" placeholder="Date" />
-                        <input className=" px-2 rounded-md w-[340px] py-5 " type="time" placeholder="time" />
+                    <div className="grid mr-4 grid-cols-2 gap-4  ">
+                        <input className=" text-white  mmc pl-2 rounded-md w-[340px] py-5 " type="text" placeholder="name" />
+                        <input className=" text-white  mmc pl-2 rounded-md w-[340px] py-5 " type="text" placeholder="email" />
+                        <input className="  text-white mmc pl-2 rounded-md w-[340px] py-5 " type="text" placeholder="Mobile number" />
+                        <input className="mmc  text-white  pl-2 rounded-md w-[340px] py-5 " type="text" placeholder="Doctor Name" />
+                        <input className="mmc  text-white px-2 rounded-md w-[340px] py-5   " type="date" placeholder="Date" />
+                        <input className="mmc px-2 rounded-md w-[340px] py-5  text-white " type="time" placeholder="time" />
 
                         <button type="submit" className=" bg-[#F7A582] text-center text-white w-full py-3">
                             Book Now
